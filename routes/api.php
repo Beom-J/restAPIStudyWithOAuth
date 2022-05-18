1bb30b0749260fb4fe2d77ee8486b03cf4d53362
Beom-J <beom00032@gmail.com>
2022-05-18 13:30:58 +0900

    feat: test 위해 api/login 에 'login' 이름 지정

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware("auth:api")->get("/user", function (Request $request) {
 });
 
 Route::post("/register", "API\AuthController@register");
-Route::post("/login", "API\AuthController@login");
+Route::post("/login", "API\AuthController@login")->name('login');
 
 Route::apiResource("/product", "API\ProductController")->middleware("auth:api");
